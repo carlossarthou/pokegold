c1f8f11eb255b76b76e7639ebb9a778bef5675a7
carlossarthou <38755745+carlossarthou@users.noreply.github.com>
2023-10-06 01:01:56 +0800

    Rework XP system - Delete unused labels in WRAM
    https://github.com/pret/pokecrystal/wiki/Generation-6-Experience-System

--- ram/wram.asm
+++ ram/wram.asm
@@ -984,12 +984,7 @@ wLastEnemyMove:: db
 wPlayerFutureSightCount:: db
 wEnemyFutureSightCount:: db
 
-wGivingExperienceToExpShareHolders:: db
-
-wBackupEnemyMonBaseStats:: ds NUM_EXP_STATS
-wBackupEnemyMonCatchRate:: db
-wBackupEnemyMonBaseExp:: db
-
+	ds 8
 wPlayerFutureSightDamage:: dw
 wEnemyFutureSightDamage:: dw
 wPlayerRageCounter:: db
